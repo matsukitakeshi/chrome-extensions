cf11a2f9a869dfcbab30d2d693f876ecd4d1e08a
matsuki_takeshi <takeshi.matsuki@genialtech.io>
2023-01-07 18:37:34 +0900

    feat: 出勤自動報告

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # chrome-extensions
 google chrome extensions
+
+## auto-attend
+マネーフォワードの出勤ボタンを押下するとスラックに本日の出勤時間予定を送信する拡張機能です。
---
+++ auto-attend/attend.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', function () {
+    const buttons = document.getElementsByClassName('time-stamp-button')
+    let attend_button = null
+
+    for (let i = 0; i < buttons.length; i++) {
+        if (buttons[i].textContent.indexOf('出勤') > 0) {
+            attend_button = buttons[i]
+        }
+    }
+
+    attend_button.addEventListener('click', function () {
+        const date = new Date();
+        const start_at = date.getHours() + ":" + date.getMinutes();
+        const finish_at = (date.getHours() + 8) + ":" + date.getMinutes();
+        const message = "出勤しました\n" + start_at + '-' + finish_at;
+
+        const channel = 'チャンネル名を入力';
+        const token = "tokenを入力"
+
+        const request = new XMLHttpRequest();
+        request.open('POST', 'https://slack.com/api/chat.postMessage', true);
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        request.send(`token=${token}&channel=${channel}&text=${message}`);
+    })
+});
